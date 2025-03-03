7fb0f37a09a3219a97c1d35b7040c8c2aedef8d9
YajselST <musicienvie@gmail.com>
2025-03-03 08:52:22 -0600

    Tipos de datos en JS

---
+++ 1-fundamentos-js/tipos-de-datos.js
@@ -0,0 +1,25 @@
+//Intod a JS, Tipos de datos
+console.log( 'hola mundo' )
+
+//Agregar diferentes tipos de datos para imprimir en consola, ctypeof command
+console.log( typeof "redonda" )
+
+//Example
+console.log( "El tipo de dato false es:", typeof false )
+
+//Typeof
+console.log( "El tipo de dato 42 es:", typeof 42 )
+
+console.log( "El tipo de dato veinticinco es:", typeof "veinticinco" )
+
+console.log( "El tipo de dato -666 es:", typeof -666 )
+
+console.log( "El tipo de dato true es:", typeof true )
+
+console.log( "El tipo de dato 0 es:", typeof 0 )
+
+console.log( "El tipo de dato '' es:", typeof '' )
+
+console.log( "El tipo de dato null es:", typeof null )
+
+console.log( "El tipo de dato undefined es:", typeof undefined )
